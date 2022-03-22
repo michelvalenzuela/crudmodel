56bb3fe3db9daa44998da8bf1c93723cb5fcc5b3
michel <michel.valenzuela.bravo@gmail.com>
2022-03-21 22:14:17 -0300

    desafio crud

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,109 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  # GET /posts
+  # GET /posts.json
+  def index
+    #@posts = Post.all
+    @post = Post.new
+
+    if params[:search]
+      @posts = Post.where('title LIKE ?', "%#{params[:search]}%").or(Post.where('content LIKE ?', "%#{params[:search]}%"))
+      
+      respond_to do |format|
+        format.js
+      end
+    else
+      @posts = Post.all
+    end
+
+  end
+
+  # GET /posts/1
+  # GET /posts/1.json
+  def show
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts
+  # POST /posts.json
+  #def create
+  #  @post = Post.new(post_params)
+#
+  #  respond_to do |format|
+  #    if @post.save
+  #      format.html { redirect_to @post, notice: 'Post was successfully created.' }
+  #      format.json { render :show, status: :created, location: @post }
+  #    else
+  #      format.html { render :new }
+  #      format.json { render json: @post.errors, status: :unprocessable_entity }
+  #    end
+  #  end
+  #end
+
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+        format.js
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.js
+      end
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  # PATCH/PUT /posts/1.json
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        @posts = Post.all
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+        format.js
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.js
+      end
+    end
+  end
+
+  # DELETE /posts/1
+  # DELETE /posts/1.json
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+      format.js
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+end
